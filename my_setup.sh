96ee4f1ee1c5c21ee50e5c7ab02bd1c135bb1238
Marcus Chan <wai-leung-marc.chan@epitech.eu>
2020-08-11 12:44:27 +0200

    Added snapd package and some minor changes

--- my_setup.sh
+++ my_setup.sh
@@ -10,7 +10,7 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 
 # Install packages
-sudo apt install git curl vim zsh vlc deluge qbittorrent blender htop neofetch krita nmap gnome-tweaks fonts-firacode chrome-gnome-shell dconf-editor gimp tmux pidgin i3lock scrot imagemagick xautolock -y
+sudo apt install git curl vim zsh vlc deluge qbittorrent blender htop neofetch krita nmap gnome-tweaks fonts-firacode chrome-gnome-shell dconf-editor gimp tmux pidgin i3lock scrot imagemagick snapd -y
 
 # Vundle (Vim)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -33,10 +33,17 @@ wget https://github.com/atom/atom/releases/latest/download/atom-amd64.deb -P ~/D
 sudo apt install ~/Downloads/atom-amd64.deb -y
 rm ~/Downloads/atom-amd64.deb
 
-# Oracle VM VirtualBox
+# Oracle VM VirtualBox (Ubuntu 18.04)
 wget https://download.virtualbox.org/virtualbox/6.1.12/virtualbox-6.1_6.1.12-139181~Ubuntu~bionic_amd64.deb -P ~/Downloads
 sudo apt install ~/Downloads/virtualbox-6.1_6.1.12-139181~Ubuntu~bionic_amd64.deb -y
 rm ~/Downloads/virtualbox-6.1_6.1.12-139181~Ubuntu~bionic_amd64.deb
+
+# Oracle VM Virtualbox (Ubuntu 20.04)
+wget https://download.virtualbox.org/virtualbox/6.1.12/virtualbox-6.1_6.1.12-139181~Ubuntu~eoan_amd64.deb -P ~/Downloads
+sudo apt install ~/Downloads/virtualbox-6.1_6.1.12-139181~Ubuntu~eoan_amd64.deb -y
+rm ~/Downloads/virtualbox-6.1_6.1.12-139181~Ubuntu~eoan_amd64.deb
+
+# Oracle VM VirtualBox Extension Pack
 wget https://download.virtualbox.org/virtualbox/6.1.12/Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack -P ~/Downloads
 yes | sudo vboxmanage extpack install ~/Downloads/Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
 rm ~/Downloads/Oracle_VM_VirtualBox_Extension_Pack-6.1.12.vbox-extpack
@@ -46,12 +53,10 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -
 sudo apt install ~/Downloads/google-chrome-stable_current_amd64.deb -y
 rm ~/Downloads/google-chrome-stable_current_amd64.deb
 
-# Snapcraft
-sudo snap install discord
-sudo snap install spotify
-sudo snap install sublime-text --classic
-sudo snap install sublime-merge --classic
-sudo snap install code --classic
+# NordVPN
+wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb -P ~/Downloads
+sudo apt install ~/Downloads/nordvpn-release_1.0.0_all.deb -y
+rm ~/Downloads/nordvpn-release_1.0.0_all.deb
 
 # Stacer
 sudo add-apt-repository ppa:oguzhaninan/stacer -y
@@ -74,6 +79,25 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
+
+# Snapcraft
+sudo snap install sublime-text --classic
+sudo snap install sublime-merge --classic
+
+# Spotify
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update && sudo apt install spotify-client -y
+
+# Discord
+wget https://discord.com/api/download?platform=linux&format=deb -P ~/Downloads
+sudo apt install ~/Downloads/discord-*.deb -y
+rm ~/Downloads/discord-*.deb
+
+# Visual Studio Code
+wget https://go.microsoft.com/fwlink/?LinkID=760868 -P ~/Downloads
+sudo apt install ~/Downloads/code_*_amd64.deb -y
+rm ~/Downloads/code_*_amd64.deb
 
 # Display public key for GitHub
 cat ~/.ssh/id_rsa.pub
